2783725eeee738942d7d238ce869e6ad8ab41d82
Jon Westman <jon.westman@edu.edugrade.se>
2023-02-28 10:13:17 +0100

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ?
         <>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value)
+            }}
+          />
+          <Employee name="Jon" role="intern" />
+          <Employee name="Abby" role={role}/>
+          <Employee name="John" />
         </>
         :
         <p>You cannot see the employees</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,15 @@
-function Employee(){
-    return <h3>Here is an employee!</h3>;
+function Employee(props) {
+    return (
+        <>
+            <h3>Employee {props.name}</h3>
+            {props.role ? (
+            <p className="role">{props.role}</p>
+            ) : (
+            <p className="norole">No role</p>
+            )}
+        </>
+    )
+
 }
 
 export default Employee;
