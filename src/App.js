c892fb97d725069ec1d14df0225feec0fb952597
Jon Westman <jon.westman@edu.edugrade.se>
2023-03-01 18:04:41 +0100

    finish edit modal

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import './index.css';
 import Employee from './components/Employee';
 import {useState} from 'react';
-import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState(
     [
       {
+        id: 1,
         name: "Jon", 
         role: "Developer", 
         img: "https://images.pexels.com/photos/4571943/pexels-photo-4571943.jpeg"
       },
       {
+        id: 2,
         name: "Sal", 
         role: "Manager", 
         img: "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg"
       },
       {
+        id: 3,
         name: "John", 
         role: "Director of Eng.", 
         img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"
       },
       {
+        id: 4,
         name: "Melanie", 
         role: "Software Engingeer", 
         img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
       },
       {
+        id: 5,
         name: "Corey", 
         role: "The Devops Guy", 
         img: "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg"
       },
       {
+        id: 6,
         name: "Jake", 
         role: "Senior Intern", 
         img: "https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg"
       }
     ]
   );
+
+    function updateEmployee(id, newName, newRole){
+      const updatedEmployees = employees.map((employee) => {
+        if(id == employee.id){
+          return {...employee, name: newName, role: newRole}
+        }
+
+        return employee;
+      });
+      setEmployees(updatedEmployees);
+    }
 
   const showEmployees = true;
   return (
@@ -48,17 +64,19 @@ function App() {
           <input
             type="text"
             onChange={(e) => {
-              console.log(e.target.value);
               setRole(e.target.value)
             }}
           />
           <div className='flex flex-wrap justify-center'>
             {employees.map((employee) => {
-              return(<Employee 
-              key={uuidv4()}
+              return(
+              <Employee 
+              key={employee.id}
+              id={employee.id}
               name={employee.name} 
               role={employee.role}
               img={employee.img}
+              updateEmployee={updateEmployee}
               />
               );
             })}
