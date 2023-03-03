64120f748ea72e026188fa00446d460648f45c85
Jon Westman <jon.westman@edu.edugrade.se>
2023-03-03 09:22:50 +0100

    style the dictionary

--- src/pages/Dictionary.js
+++ src/pages/Dictionary.js
@@ -1,37 +1,9 @@
-import { useState, useEffect } from 'react';
+import DefinitionSearch from "../components/DefinitionSearch";
 
-export default function Dictionary(){
-    const [word, setWord] = useState('');
-    const [word2, setWord2] = useState('');
-
-    useEffect(() => {
-        console.log('State Updated ' + word);
-    },[word]);//limit what state useEffect cares about --> dependency array
-
-    useEffect(() => {
-        console.log('State Updated ' + word2);
-    },[word2]);
-
-    //no dependency array --> update for any state change
-    //empty dependency array --> execute once
-    //passing in data --> only execute when those state variables are changed
-
+export default function Dictionary() {
     return (
-        <>
-            <input
-                type="text"
-                onChange={(e) => {
-                    setWord(e.target.value);
-                }}
-            />
-            <h2>Let's get the definition for: {word}</h2>
-            <input
-                type="text"
-                onChange={(e) => {
-                    setWord2(e.target.value);
-                }}
-            />
-            <h2>Let's get the definition for: {word2}</h2>
-        </>
-    )
+        <div
+            className="flex justify-center">
+            <DefinitionSearch />
+        </div>)
 }
